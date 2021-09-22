f38c3a58c6cb3a68463876121035e962709af6cc
Leonardo Roese <leo.roese@hotmail.com>
2021-09-22 09:28:35 -0500

    feat: styled components with next and material UI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system',
+]); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    };
+    return config;
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
+import StyledButton from '../components/StyledButton';
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <StyledButton />
       </main>
 
       <footer className={styles.footer}>
